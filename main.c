5b2913d557d4caa4b9e60db3fdb363ef370db438
Saizalvaro21 <saizalvaro57@gmail.com>
2023-11-18 14:35:32 +0100

    Ya tiene forma el proyecto, necesitamos pensar como hacer ese 'lado'

--- main.c
+++ main.c
@@ -4,14 +4,26 @@
 
 
 
-int	main(){
+int	main(int argc, char *argv[]){
 	__uint8_t **arr;
 	int i;
+	int j;
 
 	arr = (int**)malloc(4*sizeof(int*));
 	//Tenemos que implementar que al inicializarlo tambien se reserve memoria para cada celda
 	set_arr(arr);
 
+
+	// lectura de valores iniciales
+	i = 0;
+	while(*argv[1])
+	{
+		while(i < 4){
+			check_clues(arr, lado?, i, ((int)(*argv[1]-'0')));
+		}
+		// Para saltarse los espacios
+		argv[1] += 2;
+	}
 	// algoritmo
 	while(!solved(arr)){
 		// lo que sea de algoritmo.h
